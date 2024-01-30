38e29a7443e3ce209d3d482669d1a81e564ff49b
Kowndinya2000 <kb1204@scarletmail.rutgers.edu>
2024-01-29 20:58:42 -0500

    add: structformer

---
+++ vil2/model/network/struct_transformer.py
@@ -0,0 +1,90 @@
+"""Pose transformer network."""
+from __future__ import annotations
+import math
+import torch
+import torch.nn as nn
+from torch.nn import Transformer
+from vil2.model.embeddings.pct import StructPointTransformerEncoderSmall, EncoderMLP, DropoutSampler
+
+
+class StructTransformer(nn.Module):
+    def __init__(
+        self,
+        pcd_input_dim,
+        pcd_output_dim,
+        use_pcd_mean_center: bool,
+        num_attention_heads: int = 2,
+        encoder_num_layers: int = 2,
+        encoder_hidden_dim: int = 256,
+        encoder_dropout: float = 0.1
+    ) -> None:
+        super().__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Encode pcd features
+        self.object_encoder = StructPointTransformerEncoderSmall(
+            output_dim=pcd_output_dim,
+            input_dim=pcd_input_dim,
+            mean_center=use_pcd_mean_center,
+        )
+        # Encode position
+        self.position_encoder = nn.Sequential(nn.Linear(3 + 3 * 3, 120))
+        self.transformer = Transformer(
+            d_model=pcd_output_dim,
+            nhead=num_attention_heads,
+            num_encoder_layers=encoder_num_layers,
+            dim_feedforward=encoder_hidden_dim,
+            dropout=encoder_dropout,
+        )
+        self.mlp = EncoderMLP(pcd_output_dim, 240, uses_pt=True)
+        self.obj_dist = DropoutSampler(pcd_output_dim, 3 + 6, dropout_rate=0.1)
+
+    def encode_cond(
+        self,
+        coord1: torch.Tensor,
+        color1: torch.Tensor,
+        coord2: torch.Tensor,
+        color2: torch.Tensor,
+    ):
+        """
+        Args:
+            coord1: (B, N, 3)
+            color1: (B, N, 3)
+            coord2: (B, M, 3)
+            color2: (B, M, 3)
+        Returns:
+            (B, N + M, C)
+        """
+        # Encode geometry1 and geometry2
+        center1, enc_pcd1 = self.object_encoder(coord1, color1)
+        center2, enc_pcd2 = self.object_encoder(coord2, color2)
+
+        enc_pcd1 = self.mlp(enc_pcd1, center1)
+        enc_pcd2 = self.mlp(enc_pcd2, center2)
+        cond_feat = torch.cat((enc_pcd1, enc_pcd2), dim=1)  # (B, N + M, C)
+        return cond_feat
+
+    def forward(
+        self,
+        coord1: torch.Tensor,
+        color1: torch.Tensor,
+        coord2: torch.Tensor,
+        color2: torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Args:
+            coord1: (B, N, 3)
+            normal1: (B, N, 3)
+            color1: (B, N, 3)
+            coord2: (B, M, 3)
+            normal2: (B, M, 3)
+            color2: (B, M, 3)
+            label1: (B, 1)
+            label2: (B, 1)
+        Returns:
+            (B, 3)
+        """
+        total_feat = self.encode_cond(coord1, color1, coord2, color2)
+        encoder_output = self.transformer(total_feat)  # (B, N + M, C)
+        x = self.obj_dist(encoder_output)  # (B, 3)
+        return x
---
+++ vil2/model/structmorp_model.py
@@ -0,0 +1,174 @@
+"""Transformer Model for multi-object relative Pose Generation"""
+from __future__ import annotations
+from typing import Any
+from lightning.pytorch.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm.auto import tqdm
+import torch
+import torch.nn as nn
+import math
+import os
+import torch
+import torch.nn.functional as F
+import lightning as L
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
+import time
+from vil2.model.network.struct_transformer import StructTransformer
+
+class LitStructPoseTransformer(L.LightningModule):
+    """Lightning module for Pose Transformer"""
+
+    def __init__(self, pose_transformer: StructTransformer, cfg=None) -> None:
+        super().__init__()
+        self.pose_transformer = pose_transformer
+        self.lr = cfg.TRAIN.LR
+        self.start_time = time.time()
+
+    def training_step(self, batch, batch_idx):
+        target_coord = batch["target_coord"].to(torch.float32)
+        target_normal = batch["target_normal"].to(torch.float32)
+        target_color = batch["target_color"].to(torch.float32)
+        target_label = batch["target_label"].to(torch.long)
+        fixed_coord = batch["fixed_coord"].to(torch.float32)
+        fixed_normal = batch["fixed_normal"].to(torch.float32)
+        fixed_color = batch["fixed_color"].to(torch.float32)
+        fixed_label = batch["fixed_label"].to(torch.long)
+        pose9d = batch["target_pose"].to(torch.float32)
+        # forward
+        pose9d_pred = self.pose_transformer(
+            target_coord, target_normal, target_color, target_label, fixed_coord, fixed_normal, fixed_color, fixed_label
+        )
+        # compute loss
+        trans_loss = F.mse_loss(pose9d_pred[:, :3], pose9d[:, :3])
+        rx_loss = F.mse_loss(pose9d_pred[:, 3:6], pose9d[:, 3:6])
+        ry_loss = F.mse_loss(pose9d_pred[:, 6:9], pose9d[:, 6:9])
+        loss = trans_loss + rx_loss + ry_loss
+        # log
+        self.log("tr_trans_loss", trans_loss, sync_dist=True)
+        self.log("tr_rx_loss", rx_loss, sync_dist=True)
+        self.log("tr_ry_loss", ry_loss, sync_dist=True)
+        # log
+        self.log("train_loss", loss, sync_dist=True)
+        elapsed_time = time.time() - self.start_time
+        self.log("train_runtime", elapsed_time, sync_dist=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        target_coord = batch["target_coord"].to(torch.float32)
+        target_normal = batch["target_normal"].to(torch.float32)
+        target_color = batch["target_color"].to(torch.float32)
+        target_label = batch["target_label"].to(torch.long)
+        fixed_coord = batch["fixed_coord"].to(torch.float32)
+        fixed_normal = batch["fixed_normal"].to(torch.float32)
+        fixed_color = batch["fixed_color"].to(torch.float32)
+        fixed_label = batch["fixed_label"].to(torch.long)
+        pose9d = batch["target_pose"].to(torch.float32)
+        # forward
+        pose9d_pred = self.pose_transformer(
+            target_coord, target_normal, target_color, target_label, fixed_coord, fixed_normal, fixed_color, fixed_label
+        )
+        # compute loss
+        trans_loss = F.mse_loss(pose9d_pred[:, :3], pose9d[:, :3])
+        rx_loss = F.mse_loss(pose9d_pred[:, 3:6], pose9d[:, 3:6])
+        ry_loss = F.mse_loss(pose9d_pred[:, 6:9], pose9d[:, 6:9])
+        loss = trans_loss + rx_loss + ry_loss
+        # log
+        self.log("te_trans_loss", trans_loss, sync_dist=True)
+        self.log("te_rx_loss", rx_loss, sync_dist=True)
+        self.log("te_ry_loss", ry_loss, sync_dist=True)
+        self.log("test_loss", loss, sync_dist=True)        
+        elapsed_time = time.time() - self.start_time
+        self.log("test_runtime", elapsed_time, sync_dist=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        target_coord = batch["target_coord"].to(torch.float32)
+        target_normal = batch["target_normal"].to(torch.float32)
+        target_color = batch["target_color"].to(torch.float32)
+        target_label = batch["target_label"].to(torch.long)
+        fixed_coord = batch["fixed_coord"].to(torch.float32)
+        fixed_normal = batch["fixed_normal"].to(torch.float32)
+        fixed_color = batch["fixed_color"].to(torch.float32)
+        fixed_label = batch["fixed_label"].to(torch.long)
+        pose9d = batch["target_pose"].to(torch.float32)
+        # forward
+        pose9d_pred = self.pose_transformer(
+            target_coord, target_normal, target_color, target_label, fixed_coord, fixed_normal, fixed_color, fixed_label
+        )
+        # compute loss
+        trans_loss = F.mse_loss(pose9d_pred[:, :3], pose9d[:, :3])
+        rx_loss = F.mse_loss(pose9d_pred[:, 3:6], pose9d[:, 3:6])
+        ry_loss = F.mse_loss(pose9d_pred[:, 6:9], pose9d[:, 6:9])
+        loss = trans_loss + rx_loss + ry_loss
+        # log
+        self.log("v_trans_loss", trans_loss, sync_dist=True)
+        self.log("v_rx_loss", rx_loss, sync_dist=True)
+        self.log("v_ry_loss", ry_loss, sync_dist=True)
+        self.log("val_loss", loss, sync_dist=True)
+        elapsed_time = time.time() - self.start_time
+        self.log("val_runtime", elapsed_time, sync_dist=True)
+        return loss
+
+    def configure_optimizers(self) -> OptimizerLRScheduler:
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+
+class StructmorpModel:
+    """Transformer Model for multi-object relative Pose Generation"""
+
+    def __init__(self, cfg, pose_transformer: StructTransformer) -> None:
+        self.cfg = cfg
+        # parameters
+        # build model
+        self.pose_transformer = LitStructPoseTransformer(pose_transformer, cfg).to(torch.float32)
+
+    def train(self, num_epochs: int, train_data_loader, val_data_loader, save_path: str):
+        # Checkpoint callback
+        checkpoint_callback = ModelCheckpoint(
+            monitor="val_loss",
+            dirpath=os.path.join(save_path, "checkpoints"),
+            filename="Structmorp_model-{epoch:02d}-{val_loss:.2f}",
+            save_top_k=3,
+            mode="min",
+        )
+        # Trainer
+        # If not mac, using ddp_find_unused_parameters_true
+        strategy = "ddp_find_unused_parameters_true" if os.uname().sysname != "Darwin" else "auto"
+        accelerator = "cuda" if torch.cuda.is_available() else "cpu"
+        os.makedirs(os.path.join(save_path, "logs"), exist_ok=True)
+        trainer = L.Trainer(
+            max_epochs=num_epochs,
+            logger=WandbLogger(name=self.experiment_name(), save_dir=os.path.join(save_path, "logs")),
+            callbacks=[checkpoint_callback],
+            strategy=strategy,
+            log_every_n_steps=5,
+            accelerator=accelerator,
+        )
+        trainer.fit(self.pose_transformer, train_dataloaders=train_data_loader, val_dataloaders=val_data_loader)
+
+    def test(self, test_data_loader, save_path: str):
+        # Trainer
+        strategy = "ddp_find_unused_parameters_true" if os.uname().sysname != "Darwin" else "auto"
+        accelerator = "cuda" if torch.cuda.is_available() else "cpu"
+        os.makedirs(os.path.join(save_path, "logs"), exist_ok=True)
+        trainer = L.Trainer(
+            logger=WandbLogger(name=self.experiment_name(), save_dir=os.path.join(save_path, "logs")),
+            strategy=strategy,
+        )
+        trainer.test(self.pose_transformer, test_dataloaders=test_data_loader, accelerator=accelerator)
+
+    def experiment_name(self):
+        noise_net_name = self.cfg.MODEL.NOISE_NET.NAME
+        init_args = self.cfg.MODEL.NOISE_NET.INIT_ARGS[noise_net_name]
+        pod = init_args["pcd_output_dim"]
+        na = init_args["num_attention_heads"]
+        ehd = init_args["encoder_hidden_dim"]
+        fpd = init_args["fusion_projection_dim"]
+        pp_str = ""
+        for points in init_args.points_pyramid:
+            pp_str += str(points) + "-"
+        usl = f"{init_args.use_semantic_label}"
+        return f"Dmorp_model_pod{pod}_na{na}_ehd{ehd}_fpd{fpd}_pp{pp_str}_usl{usl}"
